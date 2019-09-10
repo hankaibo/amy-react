22aa8ca7e8345f071b3ee5ed1dd36e014e9ecb91
hankaibo <hankaibo@hotmail.com>
2019-09-10 17:34:51 +0800

    fix:官方bug修复集成

--- src/components/HeaderSearch/index.js
+++ src/components/HeaderSearch/index.js
@@ -24,17 +24,9 @@ class HeaderSearch extends PureComponent {
     };
   }
 
-  componentWillUnmount() {
-    clearTimeout(this.timeout);
-  }
-
   onKeyDown = e => {
     if (e.key === 'Enter') {
-      const { onPressEnter } = this.props;
-      const { value } = this.state;
-      this.timeout = setTimeout(() => {
-        onPressEnter(value); // Fix duplicate onPressEnter
-      }, 0);
+      this.debouncePressEnter();
     }
   };
 
