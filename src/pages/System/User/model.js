6c9dcc36384135fc06a18398bd39cb57cbf9bac3
hankaibo <hankaibo@hotmail.com>
2020-05-08 15:29:05 +0800

    fix: 修复未清空过滤条件bug

--- src/pages/System/User/model.js
+++ src/pages/System/User/model.js
@@ -212,11 +212,6 @@ export default {
         },
       };
     },
-    clearFilter(state) {
-      return {
-        ...state,
-      };
-    },
     saveTree(state, { payload }) {
       const { tree } = payload;
       return {
@@ -243,6 +238,7 @@ export default {
         ...state,
         list: [],
         pagination: {},
+        filter: {},
       };
     },
     save(state, { payload }) {
