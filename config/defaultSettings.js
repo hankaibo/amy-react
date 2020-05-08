a445328bb05da87fe7aca0141164d26728f834a3
hankaibo <hankaibo@hotmail.com>
2020-05-08 13:21:14 +0800

    chore: 日常维护

--- config/defaultSettings.js
+++ config/defaultSettings.js
@@ -4,7 +4,6 @@ export default {
   layout: 'sidemenu', // nav menu position: sidemenu or topmenu
   contentWidth: 'Fluid', // layout of content: Fluid or Fixed, only works when layout is topmenu
   fixedHeader: false, // sticky header
-  autoHideHeader: false, // auto hide header
   fixSiderbar: true, // sticky siderbar
   colorWeak: false,
   menu: {
