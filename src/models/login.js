485d222915728646e3fd1eaeafdb79a0d658341e
hankaibo <hankaibo@hotmail.com>
2020-06-09 23:45:35 +0800

    fix: 修改登录请求失败判断

--- src/models/login.js
+++ src/models/login.js
@@ -25,27 +25,29 @@ const Model = {
       // 比如，后台数据统一封装成{code:200, message:'OK', data:Object|Array }，那你的判断就是response.code===200;
       // 当然，也有的后台接口，数据直接返回无封装，像这样{token:token,resources:['a','b']]，但请求失败是统一的格式，就可以使用请求失败的格式进行判断。
       // 在这里，我的接口，请求成功时直接返回一堆数据；请求失败时统一封装错误信息返回。
-      if (response.status !== 'error') {
-        const { token, resources } = response;
-        localStorage.setItem('jwt', token);
-        reloadAuthorized(resources);
-        const urlParams = new URL(window.location.href);
-        const params = getPageQuery();
-        let { redirect } = params;
-        if (redirect) {
-          const redirectUrlParams = new URL(redirect);
-          if (redirectUrlParams.origin === urlParams.origin) {
-            redirect = redirect.substr(urlParams.origin.length);
-            if (redirect.match(/^\/.*#/)) {
-              redirect = redirect.substr(redirect.indexOf('#') + 1);
-            }
-          } else {
-            window.location.href = '/';
-            return;
-          }
-        }
-        history.replace(redirect || '/');
+      const { apierror } = response;
+      if (apierror) {
+        return;
       }
+      const { token, resources } = response;
+      localStorage.setItem('jwt', token);
+      reloadAuthorized(resources);
+      const urlParams = new URL(window.location.href);
+      const params = getPageQuery();
+      let { redirect } = params;
+      if (redirect) {
+        const redirectUrlParams = new URL(redirect);
+        if (redirectUrlParams.origin === urlParams.origin) {
+          redirect = redirect.substr(urlParams.origin.length);
+          if (redirect.match(/^\/.*#/)) {
+            redirect = redirect.substr(redirect.indexOf('#') + 1);
+          }
+        } else {
+          window.location.href = '/';
+          return;
+        }
+      }
+      history.replace(redirect || '/');
     },
 
     *logout(_, { call }) {
