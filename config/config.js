d29274ad2dad1d4ab33d8ac12d86f15f5050ab4b
hankaibo <hankaibo@hotmail.com>
2021-01-04 20:58:30 +0800

    chore: 抽取react-dev-inspector到开发环境

--- config/config.js
+++ config/config.js
@@ -74,13 +74,5 @@ export default defineConfig({
   manifest: {
     basePath: '/',
   },
-  // https://github.com/zthxxx/react-dev-inspector
-  plugins: ['react-dev-inspector/plugins/umi/react-inspector'],
-  inspectorConfig: {
-    // loader options type and docs see below
-    exclude: [],
-    babelPlugins: [],
-    babelOptions: {},
-  },
   chainWebpack: webpackPlugin,
 });
