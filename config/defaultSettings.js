624269ebee40d7e7abc08d0bff7568a0b1f43dca
hankaibo <hankaibo@hotmail.com>
2021-03-22 22:48:20 +0800

    chore: 删除无用的地址

--- config/defaultSettings.js
+++ config/defaultSettings.js
@@ -12,7 +12,7 @@ const proSettings = {
   // eg：//at.alicdn.com/t/font_1039637_btcrd5co4w.js
   // 注意：如果需要图标多色，Iconfont 图标项目里要进行批量去色处理
   // Usage: https://github.com/ant-design/ant-design-pro/pull/3517
-  iconfontUrl: '//at.alicdn.com/t/font_1729367_orciim1m93o.js',
+  iconfontUrl: '',
 };
 
 export default proSettings;
