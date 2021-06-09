661b075a11d9f8d69ac22093c022431226313635
hankaibo <hankaibo@hotmail.com>
2021-06-09 16:23:52 +0800

    chore: 更新代码规范配置

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,15 +1,15 @@
-/** @format */
+// https://stylelint.io/
 module.exports = {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-css-modules',
-    'stylelint-config-rational-order',
-    'stylelint-config-prettier',
+  extends: ['stylelint-config-standard', 'stylelint-config-css-modules', 'stylelint-config-prettier'],
+  plugins: [
+    'stylelint-order',
+    // https://github.com/constverum/stylelint-config-rational-order/issues/10
+    'stylelint-config-rational-order/plugin',
+    'stylelint-declaration-block-no-ignored-properties',
   ],
-  plugins: ['stylelint-order', 'stylelint-declaration-block-no-ignored-properties'],
   rules: {
     'no-descending-specificity': null,
-    //https://github.com/stylelint/stylelint/issues/4114
+    // https://github.com/stylelint/stylelint/issues/4114
     'function-calc-no-invalid': null,
     'function-url-quotes': 'always',
     'font-family-no-missing-generic-family-keyword': null, // iconfont
