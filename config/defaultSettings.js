6660969c73658475ce4777bda3cae578499eb43b
hankaibo <hankaibo@hotmail.com>
2021-06-20 15:41:06 +0800

    chore: 完善信息
    完善首页信息

--- config/defaultSettings.js
+++ config/defaultSettings.js
@@ -12,7 +12,7 @@ const proSettings = {
   // eg：//at.alicdn.com/t/font_1039637_btcrd5co4w.js
   // 注意：如果需要图标多色，Iconfont 图标项目里要进行批量去色处理
   // Usage: https://github.com/ant-design/ant-design-pro/pull/3517
-  iconfontUrl: '',
+  iconfontUrl: '//at.alicdn.com/t/font_2608462_jd93txil9v.js',
 };
 
 export default proSettings;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { Card, Col, Collapse, Descriptions, List, Row, Space, Typography } from 'antd';
+import { Button, Card, Col, Collapse, Descriptions, List, Row, Space, Typography } from 'antd';
 import { GithubOutlined } from '@ant-design/icons';
 import Eraser from '@/components/Eraser';
+import IconFont from '@/components/IconFont';
 import styles from './index.less';
 
 const { Panel } = Collapse;
-const { Text, Link } = Typography;
+const { Text, Link, Title, Paragraph } = Typography;
 
 const Home = () => {
   const [logList] = useState([
@@ -15,8 +16,66 @@ const Home = () => {
       content: ['Amy系统正式发布'],
     },
   ]);
+  const [backendList] = useState(['SpringBoot', 'Apache Shire', 'JWT', 'MyBatis']);
+  const [frontendList] = useState(['React', 'Ant Design', 'Umi', 'Less']);
   return (
     <div className={styles.container}>
+      <Row gutter={16}>
+        <Col xs={24} sm={16} md={16} lg={16} xl={16}>
+          <Card title={<Title level={3}>Amy后台管理框架</Title>}>
+            <Typography>
+              <Paragraph>
+                初入行时，祖师爷“高司令”赏饭吃，一直想写套后台管理系统报答。怎奈学艺不精，丢了祖师爷的脸，此为心结。
+                后来转投布兰登大师门下，与后台同事配合工作，想趁工作不忙时提前把后台管理的工作先弄起来，谁知半年过去了，同事都没有提供一个接口，然后我就亲自下场了。
+                这便有了它。
+              </Paragraph>
+              <Paragraph>
+                <Text strong>当前版本：{logList[logList.length - 1].version}</Text>
+              </Paragraph>
+              <Paragraph>
+                <Button danger>
+                  <IconFont type="icon-rmb" />
+                  免费开源
+                </Button>
+              </Paragraph>
+              <Paragraph>
+                <Button icon={<GithubOutlined />}>
+                  <span>
+                    <Link href="https://github.com/hankaibo/amy-java/" target="_blank">
+                      后台源码
+                    </Link>
+                  </span>
+                </Button>
+                <Button icon={<GithubOutlined />}>
+                  <span>
+                    <Link href="https://github.com/hankaibo/amy-react/" target="_blank">
+                      前端源码
+                    </Link>
+                  </span>
+                </Button>
+              </Paragraph>
+            </Typography>
+          </Card>
+        </Col>
+        <Col xs={24} sm={8} md={8} lg={8} xl={8}>
+          <Card title={<Title level={3}>技术选型</Title>}>
+            <Space>
+              <List
+                size="small"
+                header={<Text strong>前端技术</Text>}
+                dataSource={frontendList}
+                renderItem={(item) => <List.Item>{item}</List.Item>}
+              />
+              <List
+                size="small"
+                header={<Text strong>后台技术</Text>}
+                dataSource={backendList}
+                renderItem={(item) => <List.Item>{item}</List.Item>}
+              />
+            </Space>
+          </Card>
+        </Col>
+      </Row>
       <Row gutter={16}>
         <Col xs={24} sm={12} md={8} lg={8} xl={8}>
           <Card title="联系信息">
@@ -24,24 +83,24 @@ const Home = () => {
               <Descriptions.Item
                 label={
                   <>
-                    <GithubOutlined />
+                    <GithubOutlined style={{ fontSize: '12px', marginRight: '8px' }} />
                     <Text>前端bug</Text>
                   </>
                 }
               >
-                <Link href="https://github.com/hankaibo/amy-react/issues">
+                <Link href="https://github.com/hankaibo/amy-react/issues" target="_blank">
                   https://github.com/hankaibo/amy-react/issues
                 </Link>
               </Descriptions.Item>
               <Descriptions.Item
                 label={
                   <>
-                    <GithubOutlined />
-                    <Text>后台bug: </Text>
+                    <GithubOutlined style={{ fontSize: '12px', marginRight: '8px' }} />
+                    <Text>后台bug</Text>
                   </>
                 }
               >
-                <Link href="https://github.com/hankaibo/amy-java/issues">
+                <Link href="https://github.com/hankaibo/amy-java/issues" target="_blank">
                   https://github.com/hankaibo/amy-java/issues
                 </Link>
               </Descriptions.Item>
